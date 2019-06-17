43f2d904bbf1db8a32e67c779d10cae6db891ffb
Andy Nguyen <brobromario@hotmail.com>
2019-06-17 18:15:49 -0400

    done initial part, still room for improvement

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import Pokemons from "./Pokemons";
-import background from "../images/background.png";
+
 export default class Home extends Component {
   render() {
     return (
       <div className="footer-seperator">
-        <div id="wrapper" style={{ background: `url(${background})` }}>
+        <div id="wrapper" >
           <div className="row">
             <div className="col">
               <Pokemons />
--- src/Components/Pokemon.js
+++ src/Components/Pokemon.js
@@ -13,7 +13,7 @@ const TYPE_COLORS = {
   fire: "E73B0C",
   flying: "A3B3F7",
   ghost: "6060B2",
-  grass: "74C236",
+  grass: "7CFC00",
   ground: "D3B357",
   ice: "A3E7FD",
   normal: "C8C4BC",
@@ -48,6 +48,7 @@ export default class Pokemon extends Component {
     genderRatioFemale: "",
     evs: "",
     hatchSteps: "",
+    habitat:"",
     themeColor: "#EF5350"
   };
 
@@ -65,8 +66,9 @@ export default class Pokemon extends Component {
       );
       const speciesData = await pokeSpecies.json();
       const poke = pokeData.data;
-      const types = poke.types.map(type => type.type.name);
-      const themeColor = `${TYPE_COLORS[types[types.length - 1]]}`;
+      let types = poke.types.map(type => type.type.name);
+      let themeColor = `${TYPE_COLORS[types[types.length - 1]]}`;
+
       console.log(poke);
       console.log(speciesData);
       let description = "A Description";
@@ -80,14 +82,42 @@ export default class Pokemon extends Component {
         return null;
       });
 
+      let capitalize_firstLetter = (string)  => string.toLowerCase().charAt(0).toUpperCase() + string.slice(1);
+      let catchRate = Math.round(speciesData.capture_rate * (100/255));
+      let femaleRate = speciesData.gender_rate;
+      let genderRatioFemale = 12.5 * femaleRate;
+      let genderRatioMale = 12.5 * (8 - femaleRate);
+     
+      let evs = poke.stats
+      .filter(stat => {
+        if (stat.effort > 0) {
+          return true;
+        }
+        return false;
+      })
+      .map(stat => `${stat.effort} ${stat.stat.name
+          .toLowerCase()
+          .split("-")
+          .map(s => capitalize_firstLetter(s))
+          .join(" ")}`
+      )
+      .join(", ");
+
+      let egg_groups = speciesData.egg_groups.map(x => capitalize_firstLetter(x.name));
+      let abilities = poke.abilities.map(x => x.ability.name.split("-").map(s => capitalize_firstLetter(s)).join(" "));
+      let habitat = capitalize_firstLetter(speciesData.habitat.name);
+      //Initial hatch counter: one must walk 255 × (hatch_counter + 1) steps before this Pokémon's egg hatches, unless utilizing bonuses like Flame Body's.
+      let hatchSteps = 255 * (speciesData.hatch_counter);
+     
+     
       if (poke.length === 0) {
         this.setState({ message: "No such Pokemon Found" });
       } else {
         this.setState({
           pokemonIndex: poke.id,
-          name: poke.name,
+          name: capitalize_firstLetter(poke.name),
           height: poke.height * 10,
-          weight: poke.weight,
+          weight: Math.round(poke.weight * (2.20462/10)),
           imageUrl: poke.sprites.front_default,
           stats: poke.stats,
           hp: poke.stats[5].base_stat,
@@ -98,7 +128,15 @@ export default class Pokemon extends Component {
           specialDefense: poke.stats[1].base_stat,
           types: poke.types,
           themeColor: themeColor,
-          description: description //speciesData.flavor_text_entries[9].flavor_text
+          description: description, //speciesData.flavor_text_entries[9].flavor_text
+          catchRate: catchRate,
+          eggGroups: egg_groups,
+          habitat: habitat,
+          hatchSteps: hatchSteps,
+          abilities: abilities,
+          genderRatioFemale:genderRatioFemale,
+          genderRatioMale:genderRatioMale,
+          evs:evs
         });
       }
     } catch (error) {
@@ -108,6 +146,37 @@ export default class Pokemon extends Component {
   };
 
   render() {
+    const gender_ratio = (
+      <div className="progress">
+        <div
+          className="progress-bar"
+          role="progressbar"
+          style={{
+            width: `${this.state.genderRatioFemale}%`,
+            backgroundColor: "#FF69B4"
+          }}
+          aria-valuenow="15"
+          aria-valuemin="0"
+          aria-valuemax="100"
+        >
+          <small>{this.state.genderRatioFemale}</small>
+        </div>
+        <div
+          className="progress-bar"
+          role="progressbar"
+          style={{
+            width: `${this.state.genderRatioMale}%`,
+            backgroundColor: "#1976d2"
+          }}
+          aria-valuenow="30"
+          aria-valuemin="0"
+          aria-valuemax="100"
+        >
+          <small>{this.state.genderRatioMale}</small>
+        </div>
+      </div>
+    );
+
     const statsData = [
       {
         stats_type: "HP",
@@ -151,7 +220,43 @@ export default class Pokemon extends Component {
         profile_type:"Weight:",
         profile_value:`${this.state.weight} lbs`,
         genderRatioTrue:false
-      }];
+      },
+      {
+        profile_type:"Capture Rate:",
+        profile_value:`${this.state.catchRate} %`,
+        genderRatioTrue:false
+      },
+      {
+        profile_type:"Gender Ratio:",
+        profile_value: gender_ratio,
+        genderRatioTrue:true
+      },
+      {
+        profile_type:"Egg Group:",
+        profile_value:`${this.state.eggGroups}`,
+        genderRatioTrue:false
+      },
+      {
+        profile_type:"Hatch Steps:",
+        profile_value:`${this.state.hatchSteps}`,
+        genderRatioTrue:false
+      },
+      {
+        profile_type:"Habitat:",
+        profile_value:`${this.state.habitat}`,
+        genderRatioTrue:false
+      },
+      {
+        profile_type:"Abilities:",
+        profile_value:`${this.state.abilities}`,
+        genderRatioTrue:false
+      },
+      {
+        profile_type:"Effort Values:",
+        profile_value:`${this.state.evs}`,
+        genderRatioTrue:false
+      }
+    ];
 
     const allStats = statsData.map((x, index) => {
       return (
@@ -165,17 +270,27 @@ export default class Pokemon extends Component {
       );
     });
 
-    const allProfile = profileData.map((x, index) => {
+    const rightProfile = profileData.slice(0, profileData.length/2).map((x, index) => {
       return (
         <Profile
-        key={index}
-        profile_type={x.profile_type}
-        profile_value={x.profile_value}
-        genderRatioTrue={x.genderRatioTrue}
-      />
+          key={index}
+          profile_type={x.profile_type}
+          profile_value={x.profile_value}
+          genderRatioTrue={x.genderRatioTrue}
+        />
       );
     });
 
+    const leftProfile = profileData.slice(profileData.length/2, profileData.length).map((x, index) => {
+      return (
+        <Profile
+          key={index}
+          profile_type={x.profile_type}
+          profile_value={x.profile_value}
+          genderRatioTrue={x.genderRatioTrue}
+        />
+      );
+    });
     return (
       <div className="col">
         <div className="card">
@@ -227,14 +342,17 @@ export default class Pokemon extends Component {
           </div>
 
           <div className="card-body">
-            <h5 class="card-title text-center">Profile</h5>
+            <h5 className="card-title text-center">Profile</h5>
             <div className="row">
               <div className="col-md-6">
-                {allProfile}
+                {rightProfile}
+              </div>
+              <div className="col-md-6">
+                {leftProfile}
               </div>
             </div>
           </div>
-          <div class="card-footer text-muted">
+          <div className="card-footer text-muted">
             Pokemon Data Obtained From{" "}
             <a href="https://pokeapi.co/" target="_blank" className="card-link">
               PokeAPI.co
